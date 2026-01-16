48d5f80d78c566df8e442f0511e28939fb25b725
Abe980 <ai_980@inbox.ru>
2026-01-16 10:39:29 +0300

    add models vacancy&comments, settings admin

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Vacancy, Comment
 
-# Register your models here.
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'status', 'author', 'created_at', 'get_updated_at')
+    list_filter = ('status', 'created_at', 'company')
+    search_fields = ('title', 'company', 'description')
+    readonly_fields = ('created_at', 'get_updated_at', 'status_changed_at')
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('title', 'company', 'description', 'author')
+        }),
+        ('Зарплата', {
+            'fields': ('min_salary', 'max_salary'),
+            'classes': ('collapse',)
+        }),
+        ('Статус и даты', {
+            'fields': ('status', 'status_changed_at', 'created_at', 'get_updated_at')
+        }),
+    )
+
+    def get_updated_at(self, obj):
+        return obj.updated_at
+    get_updated_at.short_description = 'Дата последнего изменения'
+
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        if db_field.name == 'status':
+            kwargs['choices'] = Vacancy.StatusChoices.choices
+        return super().formfield_for_dbfield(db_field, request, **kwargs)
+    
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('vacancy', 'author', 'created_at', 'short_text')
+    list_filter = ('created_at', 'author')
+    search_fields = ('text', 'vacancy__title')
+
+    def short_text(self, obj):
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+    short_text.short_description = 'Текст'
--- vacancies/models.py
+++ vacancies/models.py
@@ -1,3 +1,104 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Vacancy(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        INTERESTING = 'interesting', 'Интересно'
+        REQUEST_SENT = 'request_sent', 'Отправлен запрос'
+        IN_PROGRESS = 'in_progress', 'В работе'
+        REJECTED = 'rejected', 'Отказ'
+        NOT_INTERESTING = 'not_interesting', 'Не интересно'
+
+
+    title = models.CharField(max_length=200, verbose_name='Название вакансии')
+    company = models.CharField(max_length=100, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание', blank=True, default='')
+    min_salary = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Минимальная зарплата',
+        null=True,
+        blank=True
+    )
+    max_salary = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Максимальная зарплата',
+        null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=StatusChoices.choices,
+        default=StatusChoices.INTERESTING,
+        verbose_name='Статус'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+    status_changed_at = models.DateTimeField(
+        verbose_name='Дата изменения статуса',
+        null=True,
+        blank=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата последнего изменения'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='vacancies',
+        verbose_name='Автор'
+    )
+
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.title} {self.company}'
+    
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_status = Vacancy.objects.get(pk=self.pk).status
+            if old_status != self.status:
+                self.status_changed_at = timezone.now()
+        else:
+            self.status_changed_at = timezone.now()
+        super().save(*args, **kwargs)
+
+    @property
+    def status_display(self):
+        return self.get_status_display()
+        
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name='Текст комментария')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    vacancy = models.ForeignKey(
+        Vacancy,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Вакансия'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='vacancy_comments',
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created_at']
+
+        def __str__(self):
+            return f'Комментарий от {self.author} к вакансии {self.vacancy.title}'
